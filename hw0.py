df0f7f4a2ce9734397abecde30153eec2812b2ee
hsiehtentse <paul910260@gmail.com>
2018-05-09 17:22:24 +0800

    HW0

---
+++ hw0.py
@@ -0,0 +1,18 @@
+list = []
+list2 = []
+ch = '';
+f = open('test1.txt', 'r')
+for i in f.read():
+    list.append(i)
+#print(len(list))
+#print(list)
+for j in list:
+    if j == ' ' or j == '\n':
+        list2.append(ch)
+        ch = ''
+    else:
+        ch += j
+#print(list2)
+print(list2.count('NTU'))
+
+
